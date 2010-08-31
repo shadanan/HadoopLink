8a1f3762586f0466cb245653478900cae76abacd
David Howell <davidh@wolfram.com>
2010-08-31 19:48:49 -0400

    Test defining evaluation function

--- src/test/com/wolfram/hadoop/TestHadoopLink.java
+++ src/test/com/wolfram/hadoop/TestHadoopLink.java
@@ -1,23 +1,51 @@
 package com.wolfram.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.StringUtils;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import com.wolfram.jlink.Expr;
 import com.wolfram.jlink.MathLinkException;
 
 public class TestHadoopLink {
 
-  @Test
-  public void testLaunchKernel() {
-    Configuration conf = new Configuration();
+  Configuration conf;
+
+  @Before
+  public void setUp() {
+    conf = new Configuration();
     conf.set(HadoopLink.JLINK_PATH_KEY, "/Applications/Mathematica.app/SystemFiles/Links/JLink");
     conf.set(HadoopLink.MATH_ARGS_KEY, "-linkmode launch -linkname /Applications/Mathematica.app/Contents/MacOS/MathKernel -mathlink");
+  }
+
+  @Test
+  public void testLaunchKernel() {
     HadoopLink link = null;
     try {
       link = new HadoopLink(conf);
     } catch (MathLinkException ex) {
+      fail();
+    } finally {
+      if (link != null) { link.close(); }
+    }
+  }
+
+  @Test
+  public void testDefineEvaluationFunction() {
+    HadoopLink link = null;
+    try {
+      link = new HadoopLink(conf);
+      Expr args = new Expr(ExprUtil.toSymbol("List"),
+          new Expr[] {ExprUtil.toSymbol("k"),
+                      ExprUtil.toSymbol("v")});
+      Expr fn = new Expr(ExprUtil.toSymbol("Function"),
+          new Expr[] {args,
+                      ExprUtil.toSymbol("k")});
+      link.defineEvaluationFunction(fn);
+    } catch (MathLinkException ex) {
+      System.err.println(StringUtils.stringifyException(ex));
       fail();
     } finally {
       if (link != null) { link.close(); }
