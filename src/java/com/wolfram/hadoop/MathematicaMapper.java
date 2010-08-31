97001730c3bbb3a15dae1fbd44f52e39a59d5440
David Howell <davidh@wolfram.com>
2010-08-31 14:48:59 -0400

    Acquire a kernel connection for the mapper

--- src/java/com/wolfram/hadoop/MathematicaMapper.java
+++ src/java/com/wolfram/hadoop/MathematicaMapper.java
@@ -2,18 +2,32 @@ package com.wolfram.hadoop;
 
 import java.io.IOException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.typedbytes.TypedBytesWritable;
+import org.apache.hadoop.util.StringUtils;
+
+import com.wolfram.jlink.MathLinkException;
 
 public class MathematicaMapper
     extends Mapper<TypedBytesWritable, TypedBytesWritable,
                    TypedBytesWritable, TypedBytesWritable> {
+  private static final Log LOG = LogFactory.getLog(MathematicaMapper.class);
+
+  private HadoopLink link;
 
   @Override
   public void setup(Context context) {
-    // initialize a Mathematica kernel
+    /* Initialize a Mathematica kernel */
+    try {
+      link = new HadoopLink(context.getConfiguration());
+    } catch (MathLinkException e) {
+      LOG.error(StringUtils.stringifyException(e));
+      throw new RuntimeException("Could not start a Mathematica kernel");
+    }
   }
-  
+
   @Override
   public void map(TypedBytesWritable key, TypedBytesWritable value,
                   Context context) throws IOException, InterruptedException {
@@ -23,7 +37,8 @@ public class MathematicaMapper
 
   @Override
   public void cleanup(Context context) {
-    // shut down kernel link
+    /* Shut down Mathematica connection */
+    link.close();
   }
-  
+
 }
