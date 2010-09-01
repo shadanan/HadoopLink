4ad5d16f612e84c203117f773501e6f3ac80b382
David Howell <davidh@wolfram.com>
2010-08-31 21:01:01 -0400

    Redefining MathematicaJob for launch from front end

--- src/java/com/wolfram/hadoop/MathematicaJob.java
+++ src/java/com/wolfram/hadoop/MathematicaJob.java
@@ -1,17 +1,47 @@
 package com.wolfram.hadoop;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+
+import com.wolfram.jlink.Expr;
 
 /**
  * Job driver for a Mathematica backed map-reduce job
  */
-public class MathematicaJob extends Configured implements Tool {
+public class MathematicaJob extends Configured {
 
-  @Override
-  public int run(String[] _args) throws Exception {
+  private Job job;
 
-    return 0;
+  public MathematicaJob(Expr map, Expr reduce) throws IOException {
+    checkFunctionExpr(map);
+    checkFunctionExpr(reduce);
+
+    Configuration conf = getConf();
+    job = new Job(conf);
+    Path workingPath = job.getWorkingDirectory();
+    FileSystem fs = FileSystem.get(conf);
+    writeFunctionExpr(fs, new Path(workingPath, "mapper.m"), map);
+    writeFunctionExpr(fs, new Path(workingPath, "reducer.m"), reduce);
+
   }
 
+  private void writeFunctionExpr(FileSystem fs, Path file, Expr expr)
+      throws IOException {
+    FSDataOutputStream f = fs.create(file);
+    f.writeChars(expr.toString());
+    f.close();
+  }
+
+  private void checkFunctionExpr(Expr expr) {
+    if (!expr.head().equals("Function")) {
+      String error = String.format("%s is not a Function", expr);
+      throw new IllegalArgumentException(error);
+    }
+  }
 }
