eec53100e10362b7ebc05b3c7bf666ae21829c1d
David Howell <davidh@wolfram.com>
2010-11-04 15:07:31 -0400

    whitespace fix

--- src/Mathematica/MapReduce.m
+++ src/Mathematica/MapReduce.m
@@ -9,24 +9,51 @@ MapReduceJob[h_HadoopLink,
 			 inputPaths : {__String},
 			 outputPath_String,
 			 mapper : _Symbol | _Function,
-			 reducer : _Symbol | _Function] :=
+			 reducer : _Symbol | _Function
+			 ] :=
 	JavaBlock@Module[
-		{job, conf, mapfn, reducefn},
+		{
+			job,
+			conf,
+			mapfn,
+			reducefn
+		},
+
+		(* Ensure that Java and the Hadoop classes are properly initialized *)
 		InstallJava[];
 		If[ !jLinkInitializedForHadoopQ[], initializeJLinkForHadoop[h]];
-		job = JavaNew["com.wolfram.hadoop.MathematicaJob", name];
+
 		conf = getConf[h];
-		(* Test for Mathematica configuration in Hadoop conf *)
+
+		(* Initialize a new Mathematica map-reduce job *)
+		job = JavaNew["com.wolfram.hadoop.MathematicaJob", name];
+
+		(* Test for Mathematica configuration on cluster machines in Hadoop
+		 * conf. Technically, this only needs to be defined in the
+		 * configuration on the tasktracker nodes.
+		 *
+		 * wolfram.jlink.path:
+		 * Location of the JLink jar file in the cluster's Mathematica
+		 * installation. Typically SystemFiles/Links/JLink in the Mathematica
+		 * directory.
+		 *
+		 * wolfram.math.args:
+		 * Arguments passed in the start up of a Mathematica kernel on the
+		 * cluster.
+		 *)
 		Map[
 			If[ conf@get[#] == Null,
 				die["Please define `` in your Hadoop configuration", #]
 			]&,
 			{"wolfram.jlink.path", "wolfram.math.args"}
 		];
+
 		(* Define input paths *)
 		job@addInputPath[#]& /@ inputPaths;
+
 		(* Define output path *)
 		job@setOutputPath[outputPath];
+
 		(* Define the map function for the job *)
 		Switch[mapper,
 
