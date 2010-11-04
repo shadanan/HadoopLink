5eed300748260ffb8c4243ed9ab18272350d7037
David Howell <davidh@wolfram.com>
2010-11-04 17:15:53 -0400

    Added doc page that points to external website.

--- HadoopLink/PacletInfo.m
+++ HadoopLink/PacletInfo.m
@@ -1,1 +1,17 @@
-(* Mathematica package *)
+(* Paclet Info File *)
+
+(* created 2010/11/04*)
+
+Paclet[
+    Name -> "HadoopLink",
+    Version -> "0.0.1",
+    MathematicaVersion -> "7+",
+    Description -> "Utilities for interacting with a Hadoop distributed filesystem and running Hadoop map-reduce jobs.",
+    Creator -> "David Howell",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
