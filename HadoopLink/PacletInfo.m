a22a69d9cbbe578d9e551b26dc093710cf36db16
David Howell <davidh@wolfram.com>
2010-11-09 14:28:41 -0500

    Version bump

--- HadoopLink/PacletInfo.m
+++ HadoopLink/PacletInfo.m
@@ -4,7 +4,7 @@
 
 Paclet[
     Name -> "HadoopLink",
-    Version -> "0.2.0",
+    Version -> "0.2.1",
     MathematicaVersion -> "7+",
     Description -> "Utilities for interacting with a Hadoop distributed filesystem and running Hadoop map-reduce jobs.",
     Creator -> "David Howell",
