4cac29bede16c7bbafbad956a48cdb3f19d42c29
David Howell <davidh@wolfram.com>
2010-11-11 10:44:05 -0500

    Added java class for writing data out to sequence files

---
+++ src/java/com/wolfram/hadoop/SequenceFileExportWriter.java
@@ -0,0 +1,43 @@
+package com.wolfram.hadoop;
+
+
+import com.wolfram.jlink.Expr;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.typedbytes.TypedBytesWritable;
+
+/**
+ * Helper class to speed up export of Mathematica expressions to sequence files.
+ */
+public class SequenceFileExportWriter {
+
+  SequenceFile.Writer writer;
+  TypedBytesWritable key;
+  TypedBytesWritable value;
+
+  public SequenceFileExportWriter(Configuration conf, Path path)
+      throws Exception {
+    FileSystem fs = FileSystem.get(conf);
+    key = new TypedBytesWritable();
+    value = new TypedBytesWritable();
+    writer = SequenceFile.createWriter(fs, conf, path, TypedBytesWritable.class,
+        TypedBytesWritable.class);
+  }
+
+  public void write(Expr[][] list) throws Exception {
+    for (int i = 0; i < list.length; i++) {
+      if (list[i].length != 2) {
+        throw new IllegalArgumentException("Malformed record");
+      }
+      key.setValue(ExprUtil.fromExpr(list[i][0]));
+      value.setValue(ExprUtil.fromExpr(list[i][1]));
+      writer.append(key, value);
+    }
+  }
+
+  public void close() throws Exception {
+    writer.close();
+  }
+}
