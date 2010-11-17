320ddaaf6845bcc4f3518fb021d9eb326927a3b2
David Howell <davidh@wolfram.com>
2010-11-16 21:03:16 -0500

    Wrapping mathlink factory

--- src/java/com/wolfram/hadoop/MapReduceKernelLink.java
+++ src/java/com/wolfram/hadoop/MapReduceKernelLink.java
@@ -23,115 +23,44 @@ import com.wolfram.jlink.MathLinkFactory;
 public class MapReduceKernelLink {
   static final Log LOG = LogFactory.getLog(MapReduceKernelLink.class);
 
-  private static final Expr MR_FUNCTION = ExprUtil.toSymbol("MapReduceFunction");
-
   public static final String JLINK_PATH_KEY = "wolfram.jlink.path";
   public static final String MATH_ARGS_KEY = "wolfram.math.args";
 
-  private Configuration conf;
-  private KernelLink link;
+  public static KernelLink get(Configuration conf) throws MathLinkException {
+      /* Find and set the location of JLink.jar */
+      String jlinkPath = conf.get(JLINK_PATH_KEY);
+      if (jlinkPath == null) {
+        throw new RuntimeException("wolfram.jlink.path must be defined");
+      }
+      System.setProperty("com.wolfram.jlink.libdir", jlinkPath);
 
-  public MapReduceKernelLink(Configuration conf) throws MathLinkException {
-    this.conf = conf;
+      /* Find and set the arguments to use when starting a kernel */
+      String mathArgs = conf.get(MATH_ARGS_KEY);
+      if (mathArgs == null) {
+        throw new RuntimeException("wolfram.math.args must be defined");
+      }
 
-    /* Find and set the location of JLink.jar */
-    String jlinkPath = conf.get(JLINK_PATH_KEY);
-    if (jlinkPath == null) {
-      throw new RuntimeException("wolfram.jlink.path must be defined");
-    }
-    System.setProperty("com.wolfram.jlink.libdir", jlinkPath);
+      /* Attempt to obtain a connection to a kernel */
+      link = MathLinkFactory.createKernelLink(mathArgs);
+      link.discardAnswer();
 
-    /* Find and set the arguments to use when starting a kernel */
-    String mathArgs = conf.get(MATH_ARGS_KEY);
-    if (mathArgs == null) {
-      throw new RuntimeException("wolfram.math.args must be defined");
-    }
+      /* Register a shutdown hook to close this kernel */
+      Runtime.getRuntime().addShutdownHook(new ShutdownHook(link));
 
-    /* Attempt to obtain a connection to a kernel */
-    link = MathLinkFactory.createKernelLink(mathArgs);
-    link.discardAnswer();
-
-    /* Register a shutdown hook to close this kernel */
-    Runtime.getRuntime().addShutdownHook(new ShutdownHook(this));
-  }
-
-  /**
-   * Evaluate a Mathematica .m file in this kernel.
-   *
-   * @param context   The task context
-   * @param filename  The name of the file to evaluate
-   * @return Expr
-   * @throws IOException
-   */
-  @SuppressWarnings("rawtypes")
-  public Expr load(TaskInputOutputContext context, String filename)
-      throws IOException, MathLinkException {
-    FileSystem fs = FileSystem.get(conf);
-    Path path = context.getWorkingDirectory();
-    Path file = new Path(path, filename);
-    if (!fs.exists(file)) {
-      String error = String.format("%s not found", filename);
-      throw new IOException(error);
-    }
-    FSDataInputStream f = fs.open(file);
-    StringBuffer sb = new StringBuffer();
-    byte[] buffer = new byte[1024];
-    int bytesRead;
-    while ((bytesRead = f.read(0, buffer, 0, 1024)) > 0) {
-      byte[] chunk = new byte[bytesRead];
-      System.arraycopy(buffer, 0, chunk, 0, bytesRead);
-      sb.append(chunk);
-    }
-    f.close();
-    link.evaluate(sb.toString());
-    link.waitForAnswer();
-    return link.getExpr();
-  }
-
-  public void defineEvaluationFunction(String functionDefinition)
-      throws MathLinkException {
-    link.evaluate(functionDefinition);
-    link.waitForAnswer();
-    Expr function = link.getExpr();
-    defineEvaluationFunction(function);
-  }
-
-  public void defineEvaluationFunction(Expr function)
-      throws MathLinkException {
-    Expr assign = new Expr(new Expr(Expr.SYMBOL, "Set"),
-                           new Expr[] {MR_FUNCTION, function});
-    link.evaluate(assign);
-    link.discardAnswer();
-  }
-
-  /**
-   * Evaluate the supplied string, discarding the results.
-   * 
-   * @param s String to evaluate.
-   */
-  public void evaluate(String s) throws MathLinkException {
-    link.evaluate(s);
-    link.discardAnswer();
-  }
-
-  /**
-   * Shutdown the Mathematica kernel.
-   */
-  public void close() {
-    link.terminateKernel();
-    link.close();
+      return link;
   }
 }
 
 class ShutdownHook extends Thread {
 
-  private MapReduceKernelLink link;
+  private KernelLink link;
 
-  ShutdownHook(MapReduceKernelLink link) {
+  ShutdownHook(KernelLink link) {
     this.link = link;
   }
 
   public void run() {
+    link.terminateKernel();
     link.close();
   }
 }
