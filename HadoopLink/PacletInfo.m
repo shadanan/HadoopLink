f9da33e46ba5f8b2a6ce50ad4aa973398cf0a0b8
Shad Sharma <shads@wolfram.com>
2012-01-05 14:31:38 -0600

    version bump

--- HadoopLink/PacletInfo.m
+++ HadoopLink/PacletInfo.m
@@ -1,12 +1,12 @@
 (* Paclet Info File *)
 
-(* created 2011/12/14*)
+(* created 2012/01/05*)
 
 Paclet[
     Name -> "HadoopLink",
-    Version -> "0.4.1",
+    Version -> "0.5.0",
     MathematicaVersion -> "7+",
-    Description -> "Utilities for interacting with a Hadoop distributed filesystem and running Hadoop map-reduce jobs.",
+    Description -> "Utilities for interacting with a Hadoop distributed filesystem, running Hadoop map-reduce jobs and querying HBase.",
     Creator -> "David Howell, Shad Sharma",
     Extensions -> 
         {
